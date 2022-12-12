46b6903f2f6f47a4171a2eff6b39c26cebd1f94b
jonasc <github@bcdf.eu>
2022-12-13 00:17:30 +0100

    first switch to workspace, then move to output

--- src/bin/workspace.rs
+++ src/bin/workspace.rs
@@ -4,13 +4,13 @@ fn main() {
     let (cli, mut sway, output, workspace_exists) = initialize_workspace();
 
     // If the workspace we want to go to already exists then we can just go there.
-    // The only problem is that if the workspace does not exist yet, it will be created on the same output that is currently focused.
-    // If the output where the workspace should be created is given, then we just focus this output first so that the workspace is created there.
-    if !workspace_exists && output.is_some() {
-        sway.run_command(format!("focus output {}", output.unwrap()))
-            .expect("Cannot switch to output.");
-    }
     // Create or switch to the desired workspace.
     sway.run_command(format!("workspace {}", cli.workspace))
         .expect("Cannot switch to workspace.");
+    // The only problem is that if the workspace does not exist yet, it will be created on the same output that is currently focused.
+    // If the output where the workspace should be created is given, then we just move the workspace to this output.
+    if !workspace_exists && output.is_some() {
+        sway.run_command(format!("move workspace to '{}'", output.unwrap()))
+            .expect("Cannot switch to output.");
+    }
 }
